9b34f54085775fd70b556e4bbfa5c081fd8e68c3
zhengdechang <2473023641@qq.com>
2023-06-28 20:44:39 +0800

    layout adjustment

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,22 @@
-export const Button = ({onClick, sticky = false, children}) => {
+import { Button as MuiButton } from "@mui/material";
+
+export const Button = ({ onClick, sticky = false, children }) => {
   // Base classes
-  const baseClasses = "inline-block py-2 px-5 font-bold text-black border-2 border-black";
-  // If we want the button to be sticky, flip the colors
-  const dynamicClasses = sticky ? `border-dotted hover:border-solid` : `border-solid hover:border-dotted`;
+  // const baseClasses =
+  //   "inline-block py-2 px-5 font-bold text-black border-2 border-black";
+  // // If we want the button to be sticky, flip the colors
+  // const dynamicClasses = sticky
+  //   ? `border-dotted hover:border-solid`
+  //   : `border-solid hover:border-dotted`;
 
   // Return a styled button, where the text is passed as child prop
-  return <button onClick={onClick} className={`${baseClasses} ${dynamicClasses}`}>{children}</button>;
+  return (
+    <MuiButton
+      variant="outlined"
+      onClick={onClick}
+      // className={`${baseClasses} ${dynamicClasses}`}
+    >
+      {children}
+    </MuiButton>
+  );
 };
