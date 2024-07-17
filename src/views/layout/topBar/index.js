869b57fd8c056375ec67e9c638c33a0d24f46014
zhengdechang <2473023641@qq.com>
2024-07-17 21:47:44 +0800

    update editor ui

--- src/views/layout/topBar/index.js
+++ src/views/layout/topBar/index.js
@@ -1,11 +1,14 @@
-/*
- * @Description:
- * @Author: Devin
- * @Date: 2024-07-17 18:01:20
- */
+
 import React from "react";
 import download from "@/assets/download.svg";
 import down from "@/assets/down.svg";
+import review from "@/assets/review.svg";
+import history from "@/assets/history.svg";
+import publish from "@/assets/publish.svg";
+import share from "@/assets/share.svg";
+import ellipsis from "@/assets/ellipsis.svg";
+import left from "@/assets/left.svg";
+import logoIcon from "@/assets/logo-icon.svg"
 
 const maxDisplayCount = 3; // 最大显示的名字数量
 
@@ -24,37 +27,46 @@ const TopBar = (props) => {
   }
   const collaborators = ["AA", "cc", "aa", "ss", "aa", "a"];
 
+  const handleClick = (type) => {
+    console.log("handleClick", type);
+  }
+
+  const buttonData = [
+    { src: review, label: "Review", click: handleClick },
+    { src: history, label: "History", click: handleClick },
+    { src: publish, label: "Publish", click: handleClick },
+    { src: share, label: "Share", click: handleClick },
+    { src: download, label: "", click: handleClick },
+    { src: down, label: "", click: handleClick },
+  ];
+
   return (
     <div className="flex items-center justify-between bg-gray-100 p-2">
-      <div className="flex items-center">
-        <i className="fas fa-arrow-left text-gray-500"></i>
+      <div className="flex items-center pl-4">
         <img
-          src="https://placehold.co/20x20"
-          alt="File icon"
-          className="ml-2"
+          src={left}
+          alt=""
+          onClick={() => { }}
+          className="cursor-pointer hover:opacity-75"
+        />        <img
+          src={logoIcon}
+          alt="icon"
+          className="mx-2"
         />
-        <span className="ml-2 text-gray-700">File Name</span>
+        <span className="ml-2 text-black font-bold">Project</span>
       </div>
-      <div className="flex items-center space-x-10">
-        <div className="flex items-center bg-white  rounded-lg shadow">
-          <button className="bg-green-500 text-white px-2 py-1 rounded-l-lg">
-            Review
-          </button>
-          <button className="flex items-center text-gray-700 px-2 py-1 hover:bg-gray-200 active:bg-[#9fd5a2]">
-            History
-          </button>
-          <button className="flex items-center text-gray-700 px-2 py-1 hover:bg-gray-200 active:bg-[#9fd5a2]">
-            Publish
-          </button>
-          <button className="flex items-center text-gray-700 px-2 py-1 hover:bg-gray-200 active:bg-[#9fd5a2]">
-            Share
-          </button>
-          <button className="flex items-center text-gray-700 px-2 py-1 hover:bg-gray-200 active:bg-[#9fd5a2]">
-            <img src={download} alt="" />
-          </button>
-          <button className="flex items-center text-gray-700 px-2 py-1 hover:bg-gray-200 active:bg-[#9fd5a2]">
-            <img src={down} alt="" />
-          </button>
+      <div className="flex items-center space-x-10 mr-4">
+        <div className="flex items-center bg-white rounded-lg shadow">
+          {buttonData.map((button, index) => (
+            <button
+              key={index}
+              className={`flex items-center text-gray-700 px-2 py-1 hover:bg-gray-200 active:bg-[#9fd5a2] space-x-1 ${index === 0 ? "rounded-l-lg" : ""}`}
+              onClick={button.click}
+            >
+              <img src={button.src} alt="" className="w-4 h-4" />
+              <span>{button.label || "\u00A0"}</span> {/* 使用空格字符 */}
+            </button>
+          ))}
         </div>
         <div className="flex items-center">
           {collaborators.slice(0, maxDisplayCount).map((name, index) => (
@@ -81,11 +93,12 @@ const TopBar = (props) => {
                 marginLeft: "-0.5rem", // Adjust the overlap
               }}
             >
-              <span className="text-white text-md">...</span>
+              <img src={ellipsis} alt="" />
             </div>
           )}
         </div>
       </div>
+
     </div>
   );
 };
