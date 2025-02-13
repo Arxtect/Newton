50d733c35bfc54b5649088c197beb92a10abbf5b
Q1nghuan <3011660559@qq.com>
2025-02-14 07:34:49 +0800

    feat: add snapshot management APIs (saveSnapshot, loadSnapshot, deleteSnapshot, getSnapshotInfo)

--- src/store/useYDocStore.js
+++ src/store/useYDocStore.js
@@ -9,6 +9,7 @@ import * as FS from "domain/filesystem";
 import flatten from "lodash/flatten";
 import { user } from "./useUserStore";
 import { isArray } from "lodash";
+import { v4 as uuidv4 } from 'uuid';
 import { readFileTree } from "../domain/filesystem/queries/readFileTree"
 
 export const IGNORE_PATTERNS = [".git"];
@@ -19,7 +20,7 @@ export const IGNORE_PATTERNS = [".git"];
         (set, get) => {
           if (!userId) throw new Error("User email is required to create YDocStore");
           
-          const ydoc = new Y.Doc();
+          const ydoc = new Y.Doc({ gc: false });
           const idbPersistence = new IndexeddbPersistence(`user-doc-${userId}`, ydoc);
           idbPersistence.on("synced", () => {
             console.log(`User ${userId}'s data has been loaded from IndexedDB.`);
@@ -41,10 +42,11 @@ export const IGNORE_PATTERNS = [".git"];
                 console.error("Failed to delete IndexedDB:", event);
               };
            },
+
+           //Creating YDoc from Indexeddb.
             createYDocFromPath: async (rootPath) => {
               const projectsMap = ydoc.getMap("projectsMap"); 
               const rootNode = await readFileTree(rootPath);
-            
               const stack = [{ node: rootNode, ymap: projectsMap }];
               while (stack.length > 0) {
                 const { node, ymap } = stack.pop();
@@ -68,10 +70,85 @@ export const IGNORE_PATTERNS = [".git"];
                   const ytext = new Y.Text();
                   ytext.insert(0, fileContent.toString());
                   ymap.set(path.basename(node.filepath), ytext);
-                  console.log(path.basename(node.filepath), ytext.toString(), "ytext");
                 } 
               }
               set({ ydoc }); 
+            },
+
+            // updateYDoc: async (rootPath) => {
+            // },
+
+            // updateData: async (rootPath) => {
+            //},
+
+            // add snapshot's name (If needed)
+            saveSnapshot: async (rootPath) => {
+              const snapshotsMap = ydoc.getMap("snapshotsMap");
+              // It is best to define an unique ID for each project here, not rootPath.
+              // If the project is renamed, it will not be found
+              let snapshotMap = snapshotsMap.get(rootPath);
+              if(!snapshotMap) {
+                snapshotMap = new Y.Map();
+                snapshotsMap.set(rootPath, snapshotMap);
+              }
+              const snapshotId = uuidv4();
+              // Ensure that the current ydoc stores the current project status.
+              // updateYDoc(rootPath);
+              const snapshot = Y.snapshot(ydoc);
+              const encodedSnapshot = Y.encodeSnapshot(snapshot);
+              const creationTime = new Date();
+              snapshotMap.set(snapshotId, { encodedSnapshot, creationTime, snapshotId });
+              set({ ydoc }); 
+            }, 
+
+            loadSnapshot: async (rootPath, snapshotId) => {
+              const snapshotsMap = ydoc.getMap("snapshotsMap");
+              const snapshotMap = snapshotsMap.get(rootPath);
+              
+              if(!snapshotMap.has(snapshotId)) {
+                console.error("Illegal snapshotId!");
+                return;
+              }
+
+              const snapshotInfo = snapshotMap.get(snapshotId);
+              const { encodedSnapshot } = snapshotInfo;
+              const decodedSnapshot = Y.decodeSnapshot(encodedSnapshot);
+              const tempDoc = Y.createDocFromSnapshot(ydoc, decodedSnapshot);
+              const restoredProjectsMap = tempDoc.getMap("projectsMap");
+              const restoredProjectMap = restoredProjectsMap.get(rootPath);
+              const originalProjectsMap = ydoc.getMap("projectsMap");
+              const originalProjectMap = originalProjectsMap.get(rootPath);
+
+              ydoc.transact(() => {
+                copyYMap(restoredProjectMap, originalProjectMap)
+              });
+              set({ ydoc }); 
+              //then (Re-render the Newton interface) and (update the project data: updateData) based on the ydoc.
+            },
+
+            deleteSnapshot: async (rootPath, snapshotId) => {
+              const snapshotsMap = ydoc.getMap("snapshotsMap");
+              const snapshotMap = snapshotsMap.get(rootPath);
+              if(!snapshotMap.has(snapshotId)) {
+                console.error("Illegal snapshotId!");
+                return;
+              }
+              snapshotMap.delete(snapshotId);
+              set({ ydoc });
+            },
+
+            getSnapshotInfo: async (rootPath) => {
+              const snapshotsMap = ydoc.getMap("snapshotsMap");
+              const snapshotMap = snapshotsMap.get(rootPath);
+              if(!snapshotMap.size) {
+                return [];
+              }
+              const snapshotInfoArray = [];
+              for (const [snapshotKey, snapshotEntry] of snapshotMap) {
+                const { snapshotId, creationTime } = snapshotEntry;
+                snapshotInfoArray.push({ snapshotId, creationTime });
+              }
+              return snapshotInfoArray;
             },
           };
         },
@@ -82,4 +159,25 @@ export const IGNORE_PATTERNS = [".git"];
       )
     );
 
+    const copyYMap = (sourceMap, targetMap) => {
+      const stack = [];
+      stack.push({ source: sourceMap, target: targetMap });
+    
+      while (stack.length > 0) {
+        const { source, target } = stack.pop();
+        source.forEach((value, key) => {
+          if (value instanceof Y.Map) {
+            const newMap = new Y.Map();
+            target.set(key, newMap);
+            stack.push({ source: value, target: newMap });
+          } else if (value instanceof Y.Text) {
+            const newText = new Y.Text();
+            newText.insert(0, value.toString());
+            target.set(key, newText);
+          } else {
+            target.set(key, value);
+          }
+        });
+      }
+    };
 export const useYDocStore = (userId) => createYDocStore(userId);
